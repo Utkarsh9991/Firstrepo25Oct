8bdf2159a2defd1737d0a8cea92fcbc082184946
Utkarsh <utka321@gmail.com>
2023-10-25 06:11:49 +0000

    5 files for assignment 25oct

---
+++ login.java
@@ -0,0 +1,5 @@
+class login{
+public static void main(String[]args){
+Sytem.out.println("This is login page");
+}
+}
---
+++ product.py
@@ -0,0 +1,5 @@
+import  flask
+import num.py
+a=[1,4,5,67,7,7]
+x=min(a)
+print(x)
